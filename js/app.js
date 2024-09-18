3a5c10113fc917fd7a54a8ee398f7ad81c08c107
Ali Fasihi <im.alifasihi@gmail.com>
2024-09-18 17:29:37 +0330

    Some defects

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const crossIcon = document.querySelector(".cross-icon");
+const scrollTopBtn = document.getElementById("scrollTopBtn");
 
 hamburgerIcon.addEventListener("click", function () {
   console.log(hamburgerMenu.style.transform);
@@ -10,3 +11,18 @@ hamburgerIcon.addEventListener("click", function () {
 crossIcon.addEventListener("click", function () {
   hamburgerMenu.classList.remove("show-hamburger-menu");
 });
+
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollTopBtn.style.display = "block";
+    } else {
+        scrollTopBtn.style.display = "none";
+    }
+};
+
+// When the user clicks on the button, scroll to the top of the document smoothly
+scrollTopBtn.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
